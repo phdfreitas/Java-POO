0934666a4fef5d5191c41acd50ce8b876175f9a0
phdfreitas <pedro.hnd.freitas@gmail.com>
2021-11-15 11:04:18 -0300

    Aplicando conceito de Herança

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,39 +2,26 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 	
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
 	
-	public String getTitulo() {
-		return titulo;
+	@Override
+	public double calcularXp() {
+		return XP_PADRAO + 40;
 	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	
 	public LocalDate getData() {
 		return data;
 	}
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
+	
 	@Override
 	public String toString() {
-		return "Mentoria [titulo=" + titulo + 
-				", descricao=" + descricao + 
+		return "Mentoria [titulo=" + getTitulo() + 
+				", descricao=" + getDescricao() + 
 				", data=" + data + "]";
 	}
-	
-	
-	
-	
-	
 }
